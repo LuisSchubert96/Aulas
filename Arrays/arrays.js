a30c731f4630cfd66a266378f3b917ffcd909aa9
LuisSchubert96 <luishvianna@outlook.com>
2024-09-17 20:11:16 -0300

    salvano no github

---
+++ Arrays/arrays.js
@@ -0,0 +1,6 @@
+// Aqui ele indica o item que voce desejar, nesse caso é o item 3
+// lembrete importante as listas sempre iniciam pelo número 0!
+//                        0       1       2          3
+const listaDeCompras = ["uva","banana","abacaxi","laranja"]
+const terceiroitem = listaDeCompras[3]
+console.log(terceiroitem)
