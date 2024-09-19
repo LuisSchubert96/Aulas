7355a070a85faf8b93980d6f69648eda8ce692d0
LuisSchubert96 <luishvianna@outlook.com>
2024-09-19 20:57:07 -0300

    Create exerc1Condicionais.js

---
+++ Condicionais/exerc1Condicionais.js
@@ -0,0 +1,15 @@
+
+function compararNumeros (num1, num2){
+    if(num1 === num2){
+        return "Os números são iguais"
+    }else{
+        return "Ta diferente isso ai"
+    }
+}
+
+let num1 = 20
+let num2 = 10
+
+resultado = compararNumeros(num1,num2)
+console.log(resultado)
+    
